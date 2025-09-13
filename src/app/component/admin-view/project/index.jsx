cda0ec8b64f791e9e912dd122327297a00caabc9
Sohananazneen <sohananaz01@gmail.com>
2025-09-13 19:56:52 +0600

    project data showed

--- src/app/component/admin-view/project/index.jsx
+++ src/app/component/admin-view/project/index.jsx
@@ -32,11 +32,43 @@ export default function AdminProjectView({
   formData,
   setFormData,
   handleSaveData,
+  data,
 }) {
   // console.log(formData);
   return (
     <div className="w-full">
       <div className="bg-[#d7d7d7] shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="mb-10 space-y-6">
+          {data && data.length ? (
+            data.map((item, index) => (
+              <div
+                key={index}
+                className="bg-[#ffffff] border border-green-600 flex flex-col gap-2 p-6 rounded-lg shadow-md hover:border-green-800 transition duration-300"
+              >
+                <h2 className="text-lg font-semibold text-gray-600">
+                  Title: {item.title}
+                </h2>
+                <p className="text-lg text-gray-600">
+                  <a href={item.website} target="_blank">
+                    {item.website}
+                  </a>
+                </p>
+                <p className="text-lg text-gray-600">
+                  Technologies: {item.technologies}
+                </p>
+                <p className="text-lg text-gray-600">
+                  <a href={item.github} target="_blank">
+                    GitHub: {item.github}
+                  </a>
+                </p>
+              </div>
+            ))
+          ) : (
+            <p className="text-center text-gray-600">
+              No project data available.
+            </p>
+          )}
+        </div>
         <FormControls
           controls={controls}
           formData={formData}
